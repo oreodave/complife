4ed1def5b800c33dc6cd5a992edec8d20e5c1708
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-03-18 10:47:49 +0000

    vendor in Raylib 5.5 directly

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-CFLAGS="-Wall -Wextra -Wpedantic -Wswitch-enum -Werror -std=c23 -ggdb"
-LDFLAGS="-lm -lraylib"
+CFLAGS="-Wall -Wextra -Wpedantic -Wswitch-enum -Werror -std=c23 -I./thirdparty/raylib-5.5_linux_amd64/include/"
+LDFLAGS="-L./thirdparty/raylib-5.5_linux_amd64/lib/ -lm -l:libraylib.a"
 SRC="src/sv.c src/vec.c src/bf.c src/simulation.c src/main.c"
 OUT="main.out"
 
+BFLAGS="-O3"
+
 set -xe
 
-cc $CFLAGS -o $OUT $SRC $LDFLAGS;
+cc $CFLAGS $BFLAGS -o $OUT $SRC $LDFLAGS;
